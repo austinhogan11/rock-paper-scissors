a6ccd92baf17446dc5e1e0783e9f1becf1db5b73
Austin Hogan <austinhogandev@gmail.com>
2023-08-01 14:34:57 -0400

    01 - RPS Planning

---
+++ rps.js
@@ -0,0 +1,18 @@
+// Create a function to get CPU Choice
+    // Randomly return: rock, paper, or scissors
+    // Create a list containing options
+    // Randomly generate a number 0-2 to select an option from list
+    // Return option
+
+// Create a function to play a single round of RPS.
+    // Take in two parameters *Selections should be case-INsensitive*
+        // PlayerSelection
+        // CPU Selection
+    // Prompt user for input
+
+    // Determin winner
+
+    // Return a Result String
+
+// Create a game() function to create a 5 round RPS game
+    // Function should keep schore & report the winner & loser at the end.
